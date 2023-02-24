63b4c34788f26ea9bb19d530d4d9c8bd8fe75dc3
Akansha Mittal <amittal@keywordsstudios.com>
2023-02-24 11:17:07 +0530

    Local Space and Coordinate System added

--- Matrices/Matrices/main.cpp
+++ Matrices/Matrices/main.cpp
@@ -1,22 +1,76 @@
 #include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+#include <vector>
+#include <iostream>
+
+using namespace sf;
+using namespace std;
+
+ VertexArray createLine(int x1, int y1, int x2, int y2, Color color) {
+    VertexArray line(Lines, 2);
+    line[0].position = Vector2f(x1, y1);
+    line[1].position = Vector2f(x2, y2);
+    line[0].color = color;
+    line[1].color = color;
+    return line;
+}
+
+class LocalSpace {
+    CircleShape point;
+    VertexArray xAxis, yAxis;
+    
+public:
+    LocalSpace() {
+        point.setRadius(2.f);
+        point.setFillColor(Color::Green); 
+        point.setOrigin(point.getPosition().x + point.getRadius(), point.getPosition().y + point.getRadius());
+        point.setPosition(200, 200);
+
+        xAxis = createLine(point.getPosition().x - 50, point.getPosition().y, point.getPosition().x + 50, point.getPosition().y, Color::Red);
+        yAxis = createLine(point.getPosition().x, point.getPosition().y - 50, point.getPosition().x, point.getPosition().y + 50, Color::Blue);
+    }
+
+    void draw(RenderWindow &window) {
+        window.draw(point);
+        window.draw(xAxis);
+        window.draw(yAxis);
+    }
+};
+
+class CoordinateSystem {
+    VertexArray xAxis, yAxis;
+
+public:
+    CoordinateSystem(float x, float y) {
+        xAxis = createLine(0, y/2, x, y/2, Color::Red);
+        yAxis = createLine(x/2, 0, x/2, y, Color::Blue);
+    }
+
+    void draw(RenderWindow& window) {
+        window.draw(xAxis);
+        window.draw(yAxis);
+    }
+};
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+    RenderWindow window(VideoMode(700, 700), "SFML works!");
+    
+    LocalSpace ls;
+    CoordinateSystem cs(window.getSize().x, window.getSize().y);
 
     while (window.isOpen())
     {
-        sf::Event event;
+        Event event;
         while (window.pollEvent(event))
         {
-            if (event.type == sf::Event::Closed)
+            if (event.type == Event::Closed)
                 window.close();
         }
 
         window.clear();
-        window.draw(shape);
+        ls.draw(window);
+        cs.draw(window);
         window.display();
     }
 
